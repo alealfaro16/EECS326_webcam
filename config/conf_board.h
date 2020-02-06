f506fa1d77fa126aac722268b295b424be9b7287
Alejandro Alfaro <48205459+alealfaro16@users.noreply.github.com>
2020-02-06 15:17:55 -0600

    Add files via upload

---
+++ config/conf_board.h
@@ -0,0 +1,32 @@
+/**
+ * \file
+ *
+ * \brief User board configuration template
+ *
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+
+#ifndef CONF_BOARD_H
+#define CONF_BOARD_H
+
+
+////////////////////////*FIXME COPY-PASTED NEED TO CHANGE*///////////////////////////////////////// 
+
+/** Enable Com Port. */
+#define CONF_BOARD_UART_CONSOLE
+
+/* USART1 module is used in hardware handshaking mode. */
+#define CONF_BOARD_USART_RXD
+#define CONF_BOARD_USART_TXD
+#define CONF_BOARD_USART_CTS
+#define CONF_BOARD_USART_RTS
+
+/* Configure ADM3312 enable pin. */
+#define CONF_BOARD_ADM3312_EN
+
+
+////////////////////////*FIXME COPY-PASTED NEED TO CHANGE*///////////////////////////////////////// 
+
+#endif // CONF_BOARD_H

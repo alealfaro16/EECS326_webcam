41f5e4fb74a85849f0ca2dd22cf4ba2fc78c4d9d
Alejandro Alfaro <48205459+alealfaro16@users.noreply.github.com>
2020-02-08 13:47:13 -0600

    Update wifi.h

--- wifi.h
+++ wifi.h
@@ -9,6 +9,33 @@
 #ifndef WIFI_H_
 #define WIFI_H_
 
+/** Maximum Bytes Per Second (BPS) rate that will be forced using the CTS pin. */
+#define MAX_BPS             500
+
+/** Size of the receive buffer used by the PDC, in bytes. */
+#define BUFFER_SIZE         125
+
+/** All interrupt mask. */
+#define ALL_INTERRUPT_MASK  0xffffffff
+
+/** Timer counter frequency in Hz. */
+#define TC_FREQ             1
+
+#define STRING_EOL    "\r"
+#define STRING_HEADER "--USART Hardware Handshaking Example --\r\n" \
+"-- "BOARD_NAME" --\r\n" \
+"-- Compiled: "__DATE__" "__TIME__" --"STRING_EOL
+
+#define BOARD_ID_USART             ID_USART1
+
+#define BOARD_USART                USART1
+
+#define BOARD_USART_BAUDRATE       115200
+
+#define USART_Handler              USART1_Handler
+
+#define USART_IRQn                 USART1_IRQn
+
 void wifi_usart_handler(void);
 void process_incoming_byte_wifi(uint8_t in_byte);
 void wifi_command_response_handler(uint32_t ul_id, uint32_t ul_mask);

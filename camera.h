2d0e15ffed68db3fb8d01de40bbc5c1d254b4a39
Alejandro Alfaro <48205459+alealfaro16@users.noreply.github.com>
2020-02-23 20:26:20 -0600

    Update camera.h

--- camera.h
+++ camera.h
@@ -9,7 +9,23 @@
 #ifndef CAMERA_H_
 #define CAMERA_H_
 
+/* TWI board defines. */
+#define ID_BOARD_TWI                   ID_TWI0
+#define BOARD_TWI                      TWI0
+#define BOARD_TWI_IRQn                 TWI0_IRQn
 
+/* Image sensor board defines. */
+// Image sensor Power pin.
+#define OV_POWER_PIO                   OV_SW_OVT_PIO
+#define OV_POWER_MASK                  OV_SW_OVT_MASK
+// Image sensor VSYNC pin.
+#define OV7740_VSYNC_PIO	       OV_VSYNC_PIO
+#define OV7740_VSYNC_ID		       OV_VSYNC_ID
+#define OV7740_VSYNC_MASK              OV_VSYNC_MASK
+#define OV7740_VSYNC_TYPE              OV_VSYNC_TYPE
+// Image sensor data pin.
+#define OV7740_DATA_BUS_PIO            OV_DATA_BUS_PIO
+#define OV7740_DATA_BUS_ID             OV_DATA_BUS_ID
 
 void vsync_handler(uint32_t ul_id, uint32_t ul_mask);
 void init_vsync_interrupts(void);

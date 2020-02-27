77c6feb4c26b9f9386731df332875ad9bef8fc26
Alejandro Alfaro <48205459+alealfaro16@users.noreply.github.com>
2020-02-27 12:01:05 -0600

    hidden config file

---
+++ config/user_board.h
@@ -0,0 +1,155 @@
+/**
+ * \file
+ *
+ * \brief User board definition template
+ *
+ */
+
+ /* This file is intended to contain definitions and configuration details for
+ * features and devices that are available on the board, e.g., frequency and
+ * startup time for an external crystal, external memory devices, LED and USART
+ * pins.
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+
+#ifndef USER_BOARD_H
+#define USER_BOARD_H
+
+#include <conf_board.h>
+
+// External oscillator settings.
+// Uncomment and set correct values if external oscillator is used.
+
+// External oscillator frequency
+#define BOARD_XOSC_HZ          12000000
+
+// External oscillator type.
+//!< External clock signal
+///#define BOARD_XOSC_TYPE        XOSC_TYPE_EXTERNAL
+//!< 32.768 kHz resonator on TOSC
+//#define BOARD_XOSC_TYPE        XOSC_TYPE_32KHZ
+//!< 0.4 to 16 MHz resonator on XTALS
+//#define BOARD_XOSC_TYPE        XOSC_TYPE_XTAL
+
+// External oscillator startup time
+#define BOARD_XOSC_STARTUP_US  15625//500000
+
+/** Master clock frequency */
+#define BOARD_MCK                                       CHIP_FREQ_CPU_MAX
+
+
+/******************************* PCK0 definition
+ *********************************/
+/** PCK0 */
+#define PIN_PCK1                       (PIO_PA17_IDX)
+#define PIN_PCK1_FLAGS                 (PIO_PERIPH_B | PIO_DEFAULT)
+
+
+
+/******************************* TWI definition
+ *********************************/
+/** TWI0 data pin */
+#define PIN_TWI_TWD0                   {PIO_PA3A_TWD0, PIOA, ID_PIOA, \
+                                         PIO_PERIPH_A, PIO_DEFAULT}
+
+/** TWI0 clock pin */
+#define PIN_TWI_TWCK0                  {PIO_PA4A_TWCK0, PIOA, ID_PIOA,	\
+                                         PIO_PERIPH_A, PIO_DEFAULT}
+
+/** TWI0 Data pins definition */
+#define TWI0_DATA_GPIO                 PIO_PA3_IDX
+#define TWI0_DATA_FLAGS                (PIO_PERIPH_A | PIO_DEFAULT)
+#define TWI0_DATA_MASK                 PIO_PA3
+#define TWI0_DATA_PIO                  PIOA
+#define TWI0_DATA_ID                   ID_PIOA
+#define TWI0_DATA_TYPE                 PIO_PERIPH_A
+#define TWI0_DATA_ATTR                 PIO_DEFAULT
+
+/** TWI0 clock pins definition */
+#define TWI0_CLK_GPIO                  PIO_PA4_IDX
+#define TWI0_CLK_FLAGS                 (PIO_PERIPH_A | PIO_DEFAULT)
+#define TWI0_CLK_MASK                  PIO_PA4
+#define TWI0_CLK_PIO                   PIOA
+#define TWI0_CLK_ID                    ID_PIOA
+#define TWI0_CLK_TYPE                  PIO_PERIPH_A
+#define TWI0_CLK_ATTR                  PIO_DEFAULT
+
+/** TWI0 pins */
+#define PINS_TWI0                      PIN_TWI_TWD0, PIN_TWI_TWCK0
+
+#define ID_BOARD_TWI		               ID_TWI0
+#define BOARD_TWI			                 TWI0
+#define BOARD_TWI_IRQn		             TWI0_IRQn
+
+/******************************* Image sensor definition
+ *********************************/
+/** pin for OV_VDD */
+#define PIN_OV_RST                     { PIO_PA20, PIOA, ID_PIOA, \
+                                         PIO_OUTPUT_1, PIO_DEFAULT}
+
+/** HSYNC pin */
+#define PIN_OV_HSYNC                   { PIO_PA16, PIOA, ID_PIOA, \
+                                         PIO_INPUT, PIO_PULLUP | \
+                                         PIO_IT_RISE_EDGE }
+
+/** VSYNC pin */
+#define PIN_OV_VSYNC                   { PIO_PA15, PIOA, ID_PIOA, \
+                                         PIO_INPUT, PIO_PULLUP | \
+                                         PIO_IT_RISE_EDGE }
+
+/** OV_RST pin definition */
+#define OV_RST_GPIO                    PIO_PA20_IDX
+#define OV_RST_FLAGS                   (PIO_OUTPUT_1 | PIO_DEFAULT)
+#define OV_RST_MASK                    PIO_PA20
+#define OV_RST_PIO                     PIOA
+#define OV_RST_ID                      ID_PIOA
+#define OV_RST_TYPE                    PIO_OUTPUT_1
+
+/** OV_HSYNC pin definition */
+/**#define OV_HSYNC_GPIO                  PIO_PA16_IDX
+#define OV_HSYNC_FLAGS                 (PIO_PULLUP | PIO_IT_RISE_EDGE)
+#define OV_HSYNC_MASK                  PIO_PA16
+#define OV_HSYNC_PIO                   PIOA
+#define OV_HSYNC_ID                    ID_PIOA
+#define OV_HSYNC_TYPE                  PIO_PULLUP**/
+
+/** OV_VSYNC pin definition */
+/**#define OV_VSYNC_GPIO                  PIO_PA15_IDX
+#define OV_VSYNC_FLAGS                 (PIO_PULLUP | PIO_IT_RISE_EDGE)
+#define OV_VSYNC_MASK                  PIO_PA15
+#define OV_VSYNC_PIO                   PIOA
+#define OV_VSYNC_ID                    ID_PIOA
+#define OV_VSYNC_TYPE                  PIO_PULLUP **/
+
+/** OV Data Bus pins */
+#define OV_DATA_BUS_D0                 PIO_PA24_IDX
+#define OV_DATA_BUS_D1                 PIO_PA25_IDX
+#define OV_DATA_BUS_D2                 PIO_PA26_IDX
+#define OV_DATA_BUS_D3                 PIO_PA27_IDX
+#define OV_DATA_BUS_D4                 PIO_PA28_IDX
+#define OV_DATA_BUS_D5                 PIO_PA29_IDX
+#define OV_DATA_BUS_D6                 PIO_PA30_IDX
+#define OV_DATA_BUS_D7                 PIO_PA31_IDX
+#define OV_DATA_BUS_FLAGS              (PIO_INPUT | PIO_PULLUP)
+#define OV_DATA_BUS_MASK               (0xFF000000UL)
+#define OV_DATA_BUS_PIO                PIOA
+#define OV_DATA_BUS_ID                 ID_PIOA
+#define OV_DATA_BUS_TYPE               PIO_INPUT
+#define OV_DATA_BUS_ATTR               PIO_DEFAULT
+
+/** List of Image sensor definitions. */
+#define PINS_OV                        PIN_OV_SW_OVT, PIN_PCK1, PIN_OV_RST
+
+
+/******************************* Parallel Capture definition
+ *********************************/
+/** Parallel Capture Mode Data Enable1 */
+#define PIN_PIODCEN1                   PIO_PA15
+
+/** Parallel Capture Mode Data Enable2 */
+#define PIN_PIODCEN2                   PIO_PA16
+
+
+#endif // USER_BOARD_H

24206a94a08c9be1dfe8de81993a5bdc145c3083
Alejandro Alfaro <48205459+alealfaro16@users.noreply.github.com>
2020-02-27 12:01:40 -0600

    hidden init file

--- init.c
+++ init.c
@@ -1,6 +1,52 @@
-/*
- * init.c
+/**
+ * \file
  *
- * Created: 2/6/2020 2:10:52 PM
- *  Author: ece-lab3
- */ 
+ * \brief User board initialization template
+ *
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+
+#include <asf.h>
+#include <board.h>
+#include <conf_board.h>
+#include "camera.h"
+
+void board_init(void)
+{
+	/* This function is meant to contain board-specific initialization code
+	 * for, e.g., the I/O pins. The initialization can rely on application-
+	 * specific board configuration, found in conf_board.h.
+	 */
+	
+	/* Disable the watchdog */
+#ifndef CONF_BOARD_KEEP_WATCHDOG_AT_INIT
+	WDT->WDT_MR = WDT_MR_WDDIS;
+#endif
+
+	/* Configure TWI pins */
+
+	gpio_configure_pin(TWI0_DATA_GPIO, TWI0_DATA_FLAGS);
+	gpio_configure_pin(TWI0_CLK_GPIO, TWI0_CLK_FLAGS);
+
+
+	/* Configure PCK0 pins */
+	gpio_configure_pin(PIN_PCK1, PIN_PCK1_FLAGS);
+	
+
+	
+	/* Configure Image sensor pins */
+	gpio_configure_pin(OV_RST_GPIO, OV_RST_FLAGS);
+	gpio_configure_pin(OV_HSYNC_GPIO, OV_HSYNC_FLAGS);
+	gpio_configure_pin(OV_VSYNC_GPIO, OV_VSYNC_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D0, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D1, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D2, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D3, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D4, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D5, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D6, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D7, OV_DATA_BUS_FLAGS);
+
+}
